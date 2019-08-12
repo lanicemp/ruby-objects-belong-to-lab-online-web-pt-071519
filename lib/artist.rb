55b52670c3a1cd6ad9d4ebf8167fb05dccef9569
LaNice Powell <lanicemp@aol.com>
2019-08-12 14:12:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,12 +8,13 @@
 # 
 
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :genre
  
-  def initialize(name) 
+  def initialize(name, genre) 
     @name = name
-    #@genre = genre
+    @genre = genre
   end
  
 end
-
+ 
+drake = Artist.new("Drake", "rap")
